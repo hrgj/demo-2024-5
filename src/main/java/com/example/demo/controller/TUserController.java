570cd98eeedb8fb3b7bd19a3f5d3283ca46b4099
summer <summer@summerMBP.lan>
2024-06-02 11:09:04 +0800

    验证普通表怎么操作

---
+++ src/main/java/com/example/demo/controller/TUserController.java
@@ -0,0 +1,83 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.TUser;
+import com.example.demo.service.TUserService;
+import jakarta.annotation.Resource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * (TUser)表控制层
+ *
+ * @author makejava
+ * @since 2024-06-01 23:31:08
+ */
+@RestController
+@RequestMapping("tUser")
+public class TUserController {
+    /**
+     * 服务对象
+     */
+    @Resource
+    private TUserService tUserService;
+
+    /**
+     * 分页查询
+     *
+     * @param tUser 筛选条件
+     * @param pageRequest      分页对象
+     * @return 查询结果
+     */
+    @GetMapping
+    public ResponseEntity<Page<TUser>> queryByPage(TUser tUser, PageRequest pageRequest) {
+        return ResponseEntity.ok(this.tUserService.queryByPage(tUser, pageRequest));
+    }
+
+    /**
+     * 通过主键查询单条数据
+     *
+     * @param id 主键
+     * @return 单条数据
+     */
+    @GetMapping("{id}")
+    public ResponseEntity<TUser> queryById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(this.tUserService.queryById(id));
+    }
+
+    /**
+     * 新增数据
+     *
+     * @param tUser 实体
+     * @return 新增结果
+     */
+    @PostMapping
+    public ResponseEntity<TUser> add(TUser tUser) {
+        return ResponseEntity.ok(this.tUserService.insert(tUser));
+    }
+
+    /**
+     * 编辑数据
+     *
+     * @param tUser 实体
+     * @return 编辑结果
+     */
+    @PutMapping
+    public ResponseEntity<TUser> edit(TUser tUser) {
+        return ResponseEntity.ok(this.tUserService.update(tUser));
+    }
+
+    /**
+     * 删除数据
+     *
+     * @param id 主键
+     * @return 删除是否成功
+     */
+    @DeleteMapping
+    public ResponseEntity<Boolean> deleteById(Long id) {
+        return ResponseEntity.ok(this.tUserService.deleteById(id));
+    }
+
+}
+
---
+++ src/main/java/com/example/demo/entity/TUser.java
@@ -0,0 +1,75 @@
+package com.example.demo.entity;
+
+import java.util.Date;
+import java.io.Serializable;
+
+/**
+ * (TUser)实体类
+ *
+ * @author makejava
+ * @since 2024-06-01 23:31:08
+ */
+public class TUser implements Serializable {
+    private static final long serialVersionUID = -24501485257534829L;
+
+    private Long id;
+/**
+     * order_name
+     */
+    private String userName;
+/**
+     * price
+     */
+    private Integer age;
+/**
+     * time
+     */
+    private Date createTime;
+/**
+     * time
+     */
+    private Date updateTime;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+}
+
